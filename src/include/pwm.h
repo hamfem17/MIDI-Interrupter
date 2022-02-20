103d140366ad98a82e726ee4591334bea14f872f
hamfem17 <hamfem17@htl-kaindorf.at>
2022-02-20 22:02:44 +0100

    make timer class completely independent

--- src/include/pwm.h
+++ src/include/pwm.h
@@ -13,6 +13,11 @@
 #define OCR4B_addr 0xAA
 #define OCR5B_addr 0x12A
 
+#define TCCR1A_addr 0x80
+#define TCCR3A_addr 0x90
+#define TCCR4A_addr 0xA0
+#define TCCR5A_addr 0x120
+
 #define TCCR1B_addr 0x81
 #define TCCR3B_addr 0x91
 #define TCCR4B_addr 0xA1
@@ -23,44 +28,68 @@
 #define TCNT4_addr 0xA4
 #define TCNT5_addr 0x124
 
+#define DDRB_addr 0x04
+#define DDRE_addr 0x0D
+#define DDRH_addr 0x101
+#define DDRL_addr 0x10A
+
+#define OCNB_PORT1 DDRB_addr
+#define OCNB_PORT3 DDRE_addr
+#define OCNB_PORT4 DDRH_addr
+#define OCNB_PORT5 DDRL_addr
+
+#define COMnB1 5
+#define CSn1 1
+#define WGMn0 0
+#define WGMn3 4
+
 // Pins
 #define OC1B PB6 // Pin 12
 #define OC3B PE4 // Pin 2
 #define OC4B PH4 // Pin 7
 #define OC5B PL4 // Pin 45
 
-
 class PWMTimer
 {
 	private:
-		uint16_t top_addr;
-		uint16_t com_addr; // compare match address
-		uint16_t TCCRnB_addr;
-		uint16_t TCNTn_addr;
-		uint16_t top;
+	uint16_t ocnb_port_addr;	// xDRB
+	uint16_t ocnb_pin_addr;
+	uint16_t top_addr;			// OCRnA (TOP)
+	uint16_t com_addr; 			// OCRnB (compare match)
+	uint16_t TCCRnA_addr;
+	uint16_t TCCRnB_addr;
+	uint16_t TCNTn_addr;
+	uint16_t top;
+	uint8_t duty_cycle;
 
 	public:
-		bool isFree;
-		int freq;
+	bool isFree;
+	uint16_t freq;
 
-		PWMTimer(uint16_t _top_addr, uint16_t _com_addr, uint16_t _TCCRnB_addr, uint16_t _TCNTn_addr);
-		void start(int f);
-		void stop();
+	PWMTimer(uint16_t _ocnb_port_addr,
+			 uint16_t _ocnb_pin_addr,
+			 uint16_t _top_addr,
+			 uint16_t _com_addr,
+			 uint16_t _TCCRnA_addr,
+			 uint16_t _TCCRnB_addr, 
+			 uint16_t _TCNTn_addr);
+	void start(uint16_t f, uint8_t dc);
+	void stop();
 };
 
 namespace PWM
 {
 	namespace
     {
-		PWMTimer t1 = PWMTimer(OCR1A_addr, OCR1B_addr, TCCR1B_addr, TCNT1_addr);
-		PWMTimer t3 = PWMTimer(OCR3A_addr, OCR3B_addr, TCCR3B_addr, TCNT3_addr);
-		PWMTimer t4 = PWMTimer(OCR4A_addr, OCR4B_addr, TCCR4B_addr, TCNT4_addr);
-		PWMTimer t5 = PWMTimer(OCR5A_addr, OCR5B_addr, TCCR5B_addr, TCNT5_addr);
+		PWMTimer t1 = PWMTimer(OCNB_PORT1, OC1B, OCR1A_addr, OCR1B_addr, TCCR1A_addr, TCCR1B_addr, TCNT1_addr);
+		PWMTimer t3 = PWMTimer(OCNB_PORT3, OC3B, OCR3A_addr, OCR3B_addr, TCCR3A_addr, TCCR3B_addr, TCNT3_addr);
+		PWMTimer t4 = PWMTimer(OCNB_PORT4, OC4B, OCR4A_addr, OCR4B_addr, TCCR4A_addr, TCCR4B_addr, TCNT4_addr);
+		PWMTimer t5 = PWMTimer(OCNB_PORT5, OC5B, OCR5A_addr, OCR5B_addr, TCCR5A_addr, TCCR5B_addr, TCNT5_addr);
 	}
 
-	void start(int freq);
+	void start(uint16_t freq, uint8_t dc = 20);
 
-	void stop(int freq);
+	void stop(uint16_t freq);
 
     void init();
 }
--- src/pwm.cpp
+++ src/pwm.cpp
@@ -1,45 +1,61 @@
 #include "include/pwm.h"
 
-PWMTimer::PWMTimer(uint16_t _top_addr, uint16_t _com_addr, uint16_t _TCCRnB_addr, uint16_t _TCNTn_addr)
-{
+PWMTimer::PWMTimer(uint16_t  _ocnb_port_addr,
+				   uint16_t  _ocnb_pin_addr,
+				   uint16_t _top_addr,
+				   uint16_t _com_addr,
+				   uint16_t _TCCRnA_addr,
+				   uint16_t _TCCRnB_addr, 
+				   uint16_t _TCNTn_addr) {
+
     top_addr = _top_addr;
     com_addr = _com_addr;
     TCCRnB_addr = _TCCRnB_addr;
     TCNTn_addr = _TCNTn_addr;
+
+    // Set OCnx as output
+    _SFR_MEM8(_ocnb_port_addr) |= (1 << _ocnb_pin_addr);
+
+    // Set Phase and Frequency correct PWM Mode with OCRnA as TOP
+    _SFR_MEM16(_TCCRnA_addr) |= (1 << WGMn0);
+    _SFR_MEM16(_TCCRnB_addr) |= (1 << WGMn3);
+    
+    // Set OCnx on Compare Match when upcounting, clear when downcounting
+    _SFR_MEM16(_TCCRnA_addr) |= (1 << COMnB1);
+
     isFree = true;
 }
-void PWMTimer::start(int f)
-{
+
+void PWMTimer::start(uint16_t f, uint8_t dc) {
     isFree = false;
     freq = f;
+    duty_cycle = dc;
     top = 1000000 / f;
     _SFR_MEM16(top_addr) = top;
-    _SFR_MEM16(com_addr) = top / 2; // if this is changed, also change PWMTimer::stop()
-    _SFR_MEM8(TCCRnB_addr) |= (1 << 1);
+    _SFR_MEM16(com_addr) = top * (double)duty_cycle / 100.0;
+    _SFR_MEM8(TCCRnB_addr) |= (1 << CSn1);
 }
-void PWMTimer::stop()
-{
-    //wwhile(_SFR_MEM16(TCNTn_addr) < (top / 2)); // this waits until the output is off
-    _SFR_MEM8(TCCRnB_addr) &= ~(1 << 1);
+
+void PWMTimer::stop() {
+    while(_SFR_MEM16(TCNTn_addr) < (top * (double)duty_cycle / 100.0));
+    _SFR_MEM8(TCCRnB_addr) &= ~(1 << CSn1);
     freq = 0;
     isFree = true;
 }
-void PWM::start(int freq)
-{
-    if(t1.isFree)
-    {
-        t1.start(freq);
+
+void PWM::start(uint16_t freq, uint8_t dc) {
+    if(t1.isFree) {
+        t1.start(freq, dc);
     } else if(t3.isFree) {
-        t3.start(freq);
+        t3.start(freq, dc);
     } else if(t4.isFree) {
-        t4.start(freq);
+        t4.start(freq, dc);
     } else if(t5.isFree) {
-        t5.start(freq);
+        t5.start(freq, dc);
     }
 }
 
-void PWM::stop(int freq)
-{
+void PWM::stop(uint16_t freq) {
     if(t1.freq == freq) {
         t1.stop();
     } else if(t3.freq == freq) {
